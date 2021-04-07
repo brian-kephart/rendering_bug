3386e736dbf16b32d99cc58cc1ef523aee50f61e
Brian Kephart <briantkephart@gmail.com>
2021-04-07 01:31:13 -0500

    Show renderer info in views

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,8 +1,14 @@
-# Be sure to restart your server when you modify this file.
+  # Be sure to restart your server when you modify this file.
 
-# ActiveSupport::Reloader.to_prepare do
-#   ApplicationController.renderer.defaults.merge!(
-#     http_host: 'example.org',
-#     https: false
-#   )
-# end
+  ActiveSupport::Reloader.to_prepare do
+    # This does not work
+    # ActionText::Content.default_renderer.defaults.merge!(
+    #   http_host: 'localhost',
+    #   port: 3000
+    # )
+
+    # This works
+    # ActionText::Content.default_renderer.instance_variable_get(:@env).merge!(
+    #   'HTTP_HOST' => 'localhost:3000'
+    # )
+  end
